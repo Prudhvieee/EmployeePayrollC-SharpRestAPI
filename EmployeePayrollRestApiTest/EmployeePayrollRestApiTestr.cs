514a1ac5ef82f8719f2da9c006a42e792efab89f
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-12-01 22:30:58 +0530

    [Prudhvi] add . adding employee to  json file

--- EmployeePayrollRestApiTest/EmployeePayrollRestApiTestr.cs
+++ EmployeePayrollRestApiTest/EmployeePayrollRestApiTestr.cs
@@ -1,6 +1,7 @@
 using EmployeePayrollC_SharpRestAPI;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 using System;
 using System.Collections.Generic;
@@ -34,6 +35,23 @@ namespace EmployeePayrollRestApiTest
             {
                 Console.WriteLine($"ID: {employee.id}, Name: {employee.name}, Salary: {employee.salary}");
             }
+        }
+        [TestMethod]
+        public void OnAddingEmployee_ShouldReturnAddedEmployee()
+        {
+            //Arrange
+            RestRequest restRequest = new RestRequest("employees/", Method.POST);
+            JObject jObject = new JObject();
+            jObject.Add("name", "joe");
+            jObject.Add("Salary", "10000");
+            restRequest.AddParameter("application/json", jObject, ParameterType.RequestBody);
+            //Act
+            IRestResponse restResponse = restClient.Execute(restRequest);
+            //Assert
+            Assert.AreEqual(restResponse.StatusCode, HttpStatusCode.Created);
+            EmployeeModel dataResponse = JsonConvert.DeserializeObject<EmployeeModel>(restResponse.Content);
+            Assert.AreEqual("joe", dataResponse.name);
+            Assert.AreEqual("10000", dataResponse.salary);
         }
     }
 }
